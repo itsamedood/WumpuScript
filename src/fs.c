2919685242260c1300caf8740e3ade139ac52049
dood <74739472+itsamedood@users.noreply.github.com>
2023-06-14 17:41:08 -0400

    Wrote `read_file` function.
    Gonna work on the lexer next.

--- src/fs.c
+++ src/fs.c
@@ -11,7 +11,26 @@ char *read_file(const char *path)
 	FILE *file = fopen(path, "r");
 	if (!file) raise(1, "failed to open file: '%s'.", path);
 
-	/* need to finish lol */
+	/* Find size of file. */
+	fseek(file, 0, SEEK_END);
+	long file_size = ftell(file);
+	rewind(file);
 
-	return "HERE ARE SOME FUCKING THINGS TO TOKENIZE FFS";
+	/* Allocate memory for string. */
+	char *content = (char*)malloc((file_size+1) * sizeof(char));
+	if (!content) raise(1, "failed to allocate memory for file content.");
+
+	/* Read file content into string. */
+	size_t read_size = fread(content, sizeof(char), file_size, file);
+	if (read_size != file_size)
+	{
+		fclose(file);
+		free(content);
+		raise(1, "failed to read '%s'.", path);
+	}
+
+	content[file_size] = '\0';
+	fclose(file);
+
+	return content;
 }
